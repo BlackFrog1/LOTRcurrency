77da41c1995278f4436dd3c564f990a7c85e16b4
BlackFrog1 <blackfrog1@gmail.com>
2024-11-30 07:24:50 -0500

    Lession 4, stored property; print statement

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo = false
+    
     var body: some View {
         ZStack {
            // Background Image
@@ -77,11 +79,21 @@ struct ContentView: View {
                 Spacer()
                 
                 // Info button
-                Image(systemName: "info.circle.fill")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+              
             }
-//            .border(.blue)
+            .border(.blue)
         }
     }
 }
