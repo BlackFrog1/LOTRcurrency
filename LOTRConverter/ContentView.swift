e187b7ed9a6da141c1ebedac4562965891bafa48
BlackFrog1 <blackfrog1@gmail.com>
2024-11-30 06:53:19 -0500

    Lession 3 improve layout with images and resources

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,39 +11,77 @@ struct ContentView: View {
     var body: some View {
         ZStack {
            // Background Image
+            Image(.background)
+                .resizable(resizingMode: .stretch)
+                .ignoresSafeArea()
+            
             VStack {
-                // Prancing point image cview
+                // Prancing point image view
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency exchange text
-                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    
                 // Conversion section
                 HStack {
                     // Left Conversion section
                     VStack {
                         // Currency
                         HStack {
-                             // Curreny Image
-                            
-                            // Curreny Text
+                             // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Text field
+                        Text("Text Field")
                     }
-                    // Equakl sign
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right Conversion section
                     VStack {
                         // Currency
                         HStack {
                             // Curreny Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Curreny Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Text field
+                        Text("Text Field")
                     }
                 }
+                
+                Spacer()
+                
+                // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
