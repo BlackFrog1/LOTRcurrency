d87c2152fa8b7dc31c5f77c857a060119b9dff16
BlackFrog1 <blackfrog1@gmail.com>
2024-11-30 06:17:04 -0500

    Initial content view stack layout

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+           // Background Image
+            VStack {
+                // Prancing point image cview
+                
+                // Currency exchange text
+                
+                // Conversion section
+                HStack {
+                    // Left Conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                             // Curreny Image
+                            
+                            // Curreny Text
+                        }
+                        
+                        // Text field
+                    }
+                    // Equakl sign
+                    
+                    // Right Conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Curreny Text
+                            
+                            // Curreny Image
+                        }
+                        
+                        // Text field
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
