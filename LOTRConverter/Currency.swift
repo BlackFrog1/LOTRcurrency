21d548c1b3383ca1f6aef915bf1481ef51f4e68b
BlackFrog1 <blackfrog1@gmail.com>
2024-11-30 10:32:56 -0500

    Lesson 11: currency enum

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Collin Bennett on 11/30/24.
+//
+
+enum Currency: Double {
+    case coppperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
